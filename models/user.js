7af787bdb02002fbf08d0dd4ed7e3605d158e0cc
Stephen <7speter@gmail.com>
2021-10-11 16:57:05 -0400

    Removes mongoose model, adds Mysql2/MySQL/mdb models in user.js

--- models/user.js
+++ models/user.js
@@ -1,9 +1,41 @@
-const mongoose = require('mongoose');
+const mysql = require('mysql2/promise');
+const connection = require('../config/database').connection
 
-const UserSchema = new mongoose.Schema({
-    username: String,
-    hash: String,
-    salt: String
-});
+class User {
+    constructor(username, hash, salt) {
+        //this.user_id = user_id;
+        this.username = username;
+        this.hash = hash;
+        this.salt = salt;
+    }
 
-mongoose.model('User', UserSchema);
+    static findOne(username) {
+        //console.log(username + " from line 25 database.js")
+        return connection.execute(
+            "SELECT * FROM users WHERE username = ?", [username]
+        )
+    };
+
+    static findById(id) {
+        return connection.execute(
+            "SELECT user_id, username, hash, salt, admin FROM users WHERE user_id = ?", [id]
+        )
+    };
+
+    save() {
+        /*         return connection.execute(
+                    "INSERT INTO users (username, hash, salt) VALUES (?, ?, ?)", [this.username, this.hash, this.salt] //do i need to use 'this'?
+                ) */
+        try {
+            return connection.execute(
+                "INSERT INTO users (username, hash, salt, admin) VALUES (?, ?, ?)", [this.username, this.hash, this.salt, this.admin] //do i need to use 'this'?,
+            ).catch(e => {
+                console.log('error', e);
+            });
+        } catch (e) {
+            console.log('error', e);
+        }
+    }
+}
+
+module.exports.User = User;
