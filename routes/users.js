d2431d52ec8d6693a7386277935a5ba589c6a61e
Stephen <7speter@gmail.com>
2021-10-11 19:27:58 -0400

    Adds prelim functions to login post route  in users routes file

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,37 @@
 const mongoose = require('mongoose');
-const router = require('express').Router();   
+const router = require('express').Router();
 const User = mongoose.model('User');
 const passport = require('passport');
 const utils = require('../lib/utils');
 
 // TODO
 router.get('/protected', (req, res, next) => {
+
 });
 
 // TODO
-router.post('/login', function(req, res, next){});
+router.post('/login', function(req, res, next) {
+    const saltHash = utils.genPassword(req.body.password);
+
+    const salt = saltHash.salt;
+    const hash = saltHash.hash;
+
+    const newUser = new User({
+        username: req.body.username,
+        hash: hash,
+        salt: salt
+    })
+
+    newUser.save()
+        .then((user) => {
+            res.json({ success: true, user: user });
+        })
+        .catch(err => next(err))
+});
 
 // TODO
-router.post('/register', function(req, res, next){});
+router.post('/register', function(req, res, next) {
+
+});
 
 module.exports = router;
