483cb611af2dcedb3d9901fed8b73f70e56d3bd9
Stephen <7speter@gmail.com>
2021-10-11 19:06:52 -0400

    Adds options, strategy and rel packages to passport.js

--- config/passport.js
+++ config/passport.js
@@ -2,12 +2,33 @@ const fs = require('fs');
 const path = require('path');
 const connection = require('./database');
 const { User } = require('../models/user');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
 const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
 
-// TODO
-const options = {};
 
-// TODO
-module.exports = (passport) => {}
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: PUB_KEY,
+    algorithms: ['RS256']
+};
+
+const strategy = new JwtStrategy(options, (payload, done) => {
+
+    User.findOne({ _id: payload.sub })
+        .then((user) => {
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        })
+        .catch(err => done(err, null));
+})
+
+module.exports = (passport) => {
+    passport.use(strategy)
+
+}
