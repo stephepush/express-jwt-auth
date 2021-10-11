04a0b63221c25f17e7736fe56a8139c2ffc6cd1b
Stephen <7speter@gmail.com>
2021-10-11 16:58:01 -0400

    Removes mongoose requires, adds MySQL requires

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const User = require('mongoose').model('User');
+const connection = require('./database');
+const { User } = require('../models/user');
 
 const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
 const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
